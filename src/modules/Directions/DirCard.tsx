dab3b20386b17eb596cdf0ca39cac820c75d0df0
Bagi4 <67656353+Bagi4-source@users.noreply.github.com>
2023-06-28 11:26:34 +0300

    update styles

--- src/modules/Directions/DirCard.tsx
+++ src/modules/Directions/DirCard.tsx
@@ -17,30 +17,32 @@ export default function DirCard({index, title, subjects, text, img, set, cur}: I
     return (<UseClient>
         <>
             <div onClick={() => set(index)}
-                 className={`bg-white p-8 w-full dark:bg-zinc-100 ${open ? "pb-12" : ""} dark:text-zinc-800 cursor-pointer duration-700 ease-in-out rounded-3xl mb-4 text-2xl drop-shadow-md flex flex-col lg:flex-row justify-start`}>
+                 className={`bg-white px-8 py-4 w-full dark:bg-zinc-100 ${open ? "" : ""} dark:text-zinc-800 cursor-pointer duration-700 ease-in-out rounded-3xl mb-4 text-2xl drop-shadow-md flex flex-col lg:flex-row justify-start`}>
 
                 <div className="text-[#3364E2] text-4xl font-semibold">
                     {`0${index + 1}`}
                 </div>
-                <div className="pl-16 flex flex-col justify-between">
+                <div className="pl-16 flex flex-col justify-center">
                     <div>
-                        <p className="text-3xl font-semibold">
+                        <p className="text-2xl font-semibold leading-loose">
                             {title}
                         </p>
                         {open ? (
-                            <p className="mt-4">
+                            <p className="mt-4 text-xl">
                                 {text}
                             </p>) : ""}
                     </div>
                     {open ? (<>
                         <div className="mb-12">
-                            <p className="text-3xl my-4 font-semibold">Предметы ЕГЭ:</p>
-                            <p>{subjects}</p>
+                            <p className="text-2xl my-4 font-semibold">Предметы ЕГЭ:</p>
+                            <p className="text-xl">{subjects}</p>
                         </div>
-                        <BlueButton href={"#"} text={"Подробнее"} className={"py-4 px-8"}/></>) : ""}
+                        <BlueButton href={"#"} text={"Подробнее"} className={"py-4 px-8"}/>
+                        <br/>
+                    </>) : ""}
 
                 </div>
-                <div className="flex justify-center hidden lg:block">
+                <div className="flex-col justify-center items-center hidden lg:flex">
                     {open ? (<Image
                         src={img}
                         alt="direction"
